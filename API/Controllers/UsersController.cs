edfc3b969bcb8ac03e0c2fb05503364bbbf330b1
AZBZ34 <alexbeazley18@outlook.com>
2021-10-04 11:05:19 +1100

    128. API supports cloudinary file upload.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,53 +4,84 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Authorize]
-    public class UsersController : BaseApiController
-    {
-        private readonly IUserRepository _userRepository;
-        private readonly IMapper _mapper;
+	[Authorize]
+	public class UsersController : BaseApiController
+	{
+		private readonly IUserRepository _userRepository;
+		private readonly IMapper _mapper;
+		private readonly IPhotoService _photoService;
 
-        public UsersController(IUserRepository userRepository, IMapper mapper)
-        {
-            _userRepository = userRepository;
-            _mapper = mapper;
-        }
+		public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
+		{
+			_photoService = photoService;
+			_userRepository = userRepository;
+			_mapper = mapper;
+		}
 
-        [HttpGet]
-        public async Task <ActionResult<IEnumerable<MemberDto>>> GetUsers()
-        {
-            var users = await _userRepository.GetMembersAsync();
-            
-            return Ok(users);
-        }  
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+		{
+			var users = await _userRepository.GetMembersAsync();
 
-        [HttpGet("{username}")]
-        public async Task <ActionResult<MemberDto>> GetUser(string username)
-        {
-            return await _userRepository.GetMemberAsync(username);
-        }
+			return Ok(users);
+		}
 
-        [HttpPut]
-        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
-        {
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var user = await _userRepository.GetUserByUsernameAsync(username);
-            
-            _mapper.Map(memberUpdateDto, user);
+		[HttpGet("{username}")]
+		public async Task<ActionResult<MemberDto>> GetUser(string username)
+		{
+			return await _userRepository.GetMemberAsync(username);
+		}
 
-            _userRepository.Update(user);
+		[HttpPut]
+		public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+		{
+			var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
-            if (await _userRepository.SaveAllAsync()) return NoContent();
+			_mapper.Map(memberUpdateDto, user);
 
-            return BadRequest("Failed to update user.");
-        } 
-    }
+			_userRepository.Update(user);
+
+			if (await _userRepository.SaveAllAsync()) return NoContent();
+
+			return BadRequest("Failed to update user.");
+		}
+
+		[HttpPost("add-photo")]
+		public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+		{
+			var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+			var result = await _photoService.AddPhotoAsync(file);
+
+            if (result.Error != null) return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUri.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            if (user.Photos.Count == 0)
+            {
+                photo.IsMain = true;
+            }
+
+            user.Photos.Add(photo);
+
+            if (await _userRepository.SaveAllAsync())
+                return _mapper.Map<PhotoDto>(photo);
+
+            return BadRequest("Problem adding photo");
+		}
+	}
 }
